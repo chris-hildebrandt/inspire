c1a026bb4e57dc2b3eb9123050151d3dd018239a
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-05 14:27:10 -0600

    clock up

--- app/Controllers/DisplayController.js
+++ app/Controllers/DisplayController.js
@@ -8,12 +8,41 @@ function _drawBgImg(){
   document.body.style.backgroundSize = "cover";
 }
 
+function _drawTime(){
+  let date = new Date();
+  let h = date.getHours(); // 0 - 23
+  let m = date.getMinutes(); // 0 - 59
+  let s = date.getSeconds(); // 0 - 59
+  let session = "AM";
+  if(h == 0){
+      h = 12;
+  }
+  if(h > 12){
+      h = h - 12;
+      session = "PM";
+  }
+  // @ts-ignore
+  h = (h < 10) ? "0" + h : h;
+  // @ts-ignore
+  m = (m < 10) ? "0" + m : m;
+  // @ts-ignore
+  s = (s < 10) ? "0" + s : s;
+  let time = h + ":" + m + " " + session;
+  // @ts-ignore
+  document.getElementById("clock").innerText = time;
+  // @ts-ignore
+  document.getElementById("clock").textContent = time;
+}
+
 export class DisplayController{
 
 
   constructor(){
-    setInterval(this.getBackgroundImg, 10000)
-    ProxyState.on('backgroundImg', _drawBgImg)
+    _drawTime();
+    this.getBackgroundImg();
+    setInterval(this.getBackgroundImg, 120000);
+    ProxyState.on('backgroundImg', _drawBgImg);
+    setInterval(_drawTime, 1000);
   }
 
   async getBackgroundImg(){
@@ -24,4 +53,6 @@ export class DisplayController{
       Pop.error(error)
     }
   }
+
+  
 }
